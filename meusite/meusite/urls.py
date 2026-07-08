1fe570ed9220428930e31d819a1b1e861827fb58
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-07-08 20:07:04 -0300

    atualizacao

--- meusite/meusite/urls.py
+++ meusite/meusite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . view import index_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view),
--- meusite/tarefas/views.py
+++ meusite/tarefas/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def tarefas_home(request):
-    return HttpResponse("<h1>Aqui estão suas tarefas!</h1>")
+    contexto = {
+        'nome': 'Bruno'
+    }
+    return render(request, 'pagetarefas/home.html', contexto)
 
 def tarefas_adicionar(request):
-    return HttpResponse("<h1>Adicionar Tarefa</h1> ")
+    return render(request, 'pagetarefas/adicionar.html')
